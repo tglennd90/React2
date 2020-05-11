98cc4a32abe6fcc873b8564654b66b92c3e0d679
Tatum Daily <tgd9019@outlook.com>
2020-05-11 18:18:52 -0500

    Deployment Troubleshooting

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const BookPost = require('../models/BookPost');
+const BookPost = require('../models/bookPost');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
     BookPost.find({ })
-        .then((data) => {
-            // console.log('Data: ', data);
-            res.json(data);
-        })
-        .catch((error) => {
-            console.log("Error: ", error)
-        });
+    .then((data) => {
+        // console.log('Data: ', data);
+        res.json(data);
+    })
+    .catch((error) => {
+        console.log("Error: ", error)
+    });
 
 });
 
